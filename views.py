0f75b1c7e68442fbe3db4588e73516038e3243fe
Katrina Sradford <kstrad7@wgu.edu>
2024-02-03 14:50:14 -0500

    create flask app and sample routing

---
+++ views.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, render_template
+
+views = Blueprint(__name__, "views")
+
+
+'''
+suggested routing for planned pages
+'''
+@views.route("/")
+def home():
+    return render_template("index.html")
+
+@views.route("/links")
+def links():
+    return "suspicious links/urls"
+
+@views.route("/privacy")
+def privacy():
+    return "social media/ai"
+
+@views.route("/strangers")
+def strangers():
+    return "good practices when talking to strangers"
+
+@views.route("/protection")
+def protection():
+    return "protecting important personal information"
+
+@views.route("/quiz")
+def quiz():
+    return "quiz"
